9626f5fbad05445626da77fefe7a7be7d646754f
D3nams <dunamarufus@outlook.com>
2025-10-07 20:56:58 -0700

    Add ads.txt and route for AdSense verification

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_from_directory
 from extractor import extract_video_data_from_url
 
 app = Flask(__name__)
@@ -6,6 +6,10 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/ads.txt')
+def ads_txt():
+    return send_from_directory(app.root_path, 'ads.txt')
 
 @app.route('/download', methods=['POST'])
 def download():
